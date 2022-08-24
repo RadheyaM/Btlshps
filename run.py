404bb165b05058488cf3685a35db18f2e81f36db
RadheyaM <radheyamansel@hotmail.com>
2022-08-24 17:19:12 +0000

    Create a board class and check that it prints

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""Module desc goes here"""
+# from random import randint
+
+
+class Battlegrid:
+    """create a battleship board grid"""
+    def __init__(self, size, ships, player_name, type):
+        self. size = size
+        self.board = [["0" for x in range(size)] for y in range(size)]
+        self.ships = ships
+        self.player_name = player_name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+    def print_grid(self):
+        """print the grid"""
+        for row in self.board:
+            print(" ".join(row))
+
+
+player_board = Battlegrid(10, 2, "Radheya", "player")
+
+player_board.print_grid()
